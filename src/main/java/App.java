46c99e269a295cf64be326cbc5f486a5684ed829
alexe <prograbota@mail.ru>
2022-11-06 15:46:09 +0300

    ref beans HelloWord

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,9 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println("references beans HelloWord: " + (bean1 == bean2));
     }
 }
